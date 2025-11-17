c6a9b66047c6a86c0f2ce9434b2d5ca194d6b704
chamodij <chamodijayasinghe23@gmail.com>
2025-11-17 22:03:16 +0530

    new commit

--- Hotel_Booking_System/src/pages/Home.jsx
+++ Hotel_Booking_System/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Hero from '../components/Hero';
 import FeaturedDestination from '../components/FeaturedDestination';
 import ExclusiveOffers from '../components/ExclusiveOffers';
 import Testimonial from '../components/Testimonial';
+import NewsLetter from '../components/NewsLetter';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
@@ -10,7 +12,9 @@ const Home = () => {
       <Hero />
       <FeaturedDestination/>
       <ExclusiveOffers/>
-   
+      <Testimonial/>
+      <NewsLetter/>
+      <Footer/>
     </div>
   );
 }
